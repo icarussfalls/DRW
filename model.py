7afb2ea4e1353ff5e1dbff086153380e7f85793b
icarus <pokhrel.pradip163@gmail.com>
2025-05-25 01:37:09 +0545

    added layerdrop

--- model.py
+++ model.py
@@ -4,8 +4,27 @@ import torch.nn.functional as F
 import math
 
 # Positional Encoding
+# class PositionalEncoding(nn.Module):
+#     def __init__(self, d_model: int, max_len: int = 100, dropout: float = 0.1):
+#         super().__init__()
+#         self.dropout = nn.Dropout(dropout)
+
+#         pe = torch.zeros(max_len, d_model)
+#         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+#         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+#         pe[:, 0::2] = torch.sin(position * div_term)
+#         pe[:, 1::2] = torch.cos(position * div_term)
+#         pe = pe.unsqueeze(0)  # (1, max_len, d_model)
+#         self.register_buffer('pe', pe)
+
+#     def forward(self, x):
+#         # x shape: (batch_size, seq_len, d_model)
+#         x = x + self.pe[:, :x.size(1), :].detach()
+#         return self.dropout(x)
+    
+# Positional Encoding with learnable embeddings
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model: int, max_len: int = 100, dropout: float = 0.1):
+    def __init__(self, d_model: int, max_len: int = 60, dropout: float = 0.1):
         super().__init__()
         self.dropout = nn.Dropout(dropout)
 
@@ -21,6 +40,7 @@ class PositionalEncoding(nn.Module):
         # x shape: (batch_size, seq_len, d_model)
         x = x + self.pe[:, :x.size(1), :].detach()
         return self.dropout(x)
+
 
 # Input Embeddings
 class InputEmbeddings(nn.Module):
