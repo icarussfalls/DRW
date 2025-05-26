b0876221fb42461c9adc4e2609a8fa84fcb3bca1
icarus <pokhrel.pradip163@gmail.com>
2025-05-27 01:44:02 +0545

    added headdrop

--- tabular_transformers.py
+++ tabular_transformers.py
@@ -93,17 +93,11 @@ class TransformerEncoderLayer(nn.Module):
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=500):
         super().__init__()
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float32).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0)
-        self.register_buffer('pe', pe)
+        self.pos_embedding = nn.Parameter(torch.randn(1, max_len, d_model))  # Learnable!
     
     def forward(self, x):
-        x = x + self.pe[:, :x.size(1)]
-        return x
+        return x + self.pos_embedding[:, :x.size(1), :]
+
 
 class Pooling(nn.Module):
     def __init__(self, d_model):
